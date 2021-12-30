4e75746a20df82e083c13fa760d577cf8da268af
MAASAHEEBI <96136123+MAASAHEEBI@users.noreply.github.com>
2021-12-30 02:07:32 -0500

    ass 3.3
    python function to calculate upper and lower case alphabets in a given string.

---
+++ assignment3.3.py
@@ -0,0 +1,55 @@
+'''Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+
+Sample String : 'The quick Brow Fox'
+Expected Output :
+No. of Upper case characters : 3
+No. of Lower case Characters : 12'''
+
+ 
+##def string_test(s):
+##    d={"U_CASE":0, "L_CASE":0}
+##    for c in s:
+##        if c.isupper():
+##           d["U_CASE"]+=1
+##        elif c.islower():
+##           d["L_CASE"]+=1
+##        else:
+##           pass
+##    print ("Original String : ", s)
+##    print ("No. of Upper case characters : ", d["U_CASE"])
+##    print ("No. of Lower case Characters : ", d["L_CASE"])
+##
+##string_test('The quick Brown Fox')
+
+
+
+def string_test():
+    s=input('enter text: ')
+    d={"U_CASE":0, "L_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["U_CASE"]+=1
+        elif c.islower():
+           d["L_CASE"]+=1
+        else:
+           pass
+
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", d["U_CASE"])
+    print ("No. of Lower case Characters : ", d["L_CASE"])
+
+string_test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
